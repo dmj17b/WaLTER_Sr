f189f978c11e24b2b5d3fccd016fd0e6b7ad0960
David <dmj17b@fsu.edu>
2025-10-01 12:41:32 -0400

    Adding ros2 pkg for wheel control

---
+++ launch/boom_launch.py
@@ -0,0 +1,60 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import TimerAction
+
+def generate_launch_description():
+    # Declare launch arguments
+
+    # Create nodes for each ODrive motor
+    knee = Node(
+        package='odrive_can',
+        executable='odrive_can_node',
+        name = 'knee',
+        namespace='knee',
+        parameters = [{
+            'node_id' : 1,
+            'interface' : 'can0',
+        }] 
+    )
+    hip = Node(
+        package='odrive_can',
+        executable='odrive_can_node',
+        name = 'hip',
+        namespace='hip',
+        parameters = [{
+            'node_id' : 0,
+            'interface' : 'can0',
+        }] 
+    )
+    # Main control node 
+    # This node handles the main control loop, reading joystick inputs and mappng them to hip/knee/wheel commands
+    main_ctrl_node= Node(
+        package='main_ctrl',
+        executable='main_ctrl',
+        name='main_ctrl_node',
+        
+    )
+
+    # Joystick reader
+    # This node reads joystick inputs and publishes them to the 'joy' topic
+    joy_node = Node(
+        package='joy',
+        executable='joy_node',
+        name='joy_node',
+        parameters=[{
+            'deadzone': 0.1,
+            'dev': '/dev/input/js0',
+            'coalesce_interval': 0.05,
+        }]
+
+    )
+
+
+
+    return LaunchDescription([
+        hip,
+        knee,
+        joy_node,
+        main_ctrl_node,
+
+    ])
